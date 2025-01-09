7924bad8a411b93775e43c74663400956f309c05
Freezingn1 <edikgarr@gmail.com>
2025-01-10 01:33:27 +0300

    interface.js

--- interface.js
+++ interface.js
@@ -10,8 +10,6 @@
       this.create = function () {
         html = $("<div class=\"new-interface-info\">\n            <div class=\"new-interface-info__body\">\n                <div class=\"new-interface-info__head\"></div>\n                <div class=\"full-start-new__title\"></div>\n                <div class=\"new-interface-info__details\"></div>\n                <div class=\"new-interface-info__description\"></div>\n            </div>\n        </div>");
       };
-
-function(){"use strict";Lampa.SettingsApi.addParam({component:"interface",param:{name:"logo_glav",type:"select",values:{1:"Скрыть",0:"Отображать"},default:"0"},field:{name:"Логотипы вместо названий",description:"Отображает логотипы фильмов вместо текста"}}),window.logoplugin||(window.logoplugin=!0,Lampa.Listener.follow("full",(function(a){if("complite"==a.type&&"1"!=Lampa.Storage.get("logo_glav")){var e=a.data.movie;var o=Lampa.TMDB.api((e.name ? "tv" : "movie") + "/" + e.id + "/images?api_key=" + Lampa.TMDB.key() + "&language=" + Lampa.Storage.get("language"));$.get(o,(function(a){if(a.logos&&a.logos[0]){var e=a.logos[0].file_path;""!=e&&$(".full-start-new__title").html('<img style="margin-top: 5px;max-height: 125px;" src="'+Lampa.TMDB.image("/t/p/w300"+e.replace(".svg",".png"))+'" />')}}))}})))}();
 	    
       this.update = function (data) {
         html.find('.new-interface-info__head,.new-interface-info__details').text('---');
