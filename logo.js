b2eea66ce243bda7542e52b3c282d1860ff8080a
Freezingn1 <edikgarr@gmail.com>
2025-01-10 12:18:43 +0300

    logo.js

--- logo.js
+++ logo.js
@@ -18,13 +18,13 @@
     }),
     window.logoplugin || (window.logoplugin = !0,
     Lampa.Listener.follow("full", (function(a) {
-        if ("complite" == a.type && "1" != Lampa.Storage.get("logo_glav1")) {
+        if ("complite" == a.type && "0" != Lampa.Storage.get("logo_glav1")) {
             var e = a.data.movie;
             var o = Lampa.TMDB.api((e.name ? "tv" : "movie") + "/" + e.id + "/images?api_key=" + Lampa.TMDB.key() + "&language=" + Lampa.Storage.get("language"));
             $.get(o, (function(a) {
                 if (a.logos && a.logos[0]) {
                     var e = a.logos[0].file_path;
-                    "" != e (".items-line__title").html('<img style="margin-top: 5px;max-height: 125px;" src="' + Lampa.TMDB.image("/t/p/w300" + e.replace(".svg", ".png")) + '" />')
+                    "" != e && $(".items-line__title").html('<img style="margin-top: 5px;max-height: 125px;" src="' + Lampa.TMDB.image("/t/p/w300" + e.replace(".svg", ".png")) + '" />')
                 }
             }
             ))
