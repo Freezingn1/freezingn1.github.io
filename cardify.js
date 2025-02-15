cd946d0684e14867635aa2dae799f9cb65bebbfc
Freezingn1 <edikgarr@gmail.com>
2025-02-15 17:48:30 +0300

    cardify.js

--- cardify.js
+++ cardify.js
@@ -1,6 +1,6 @@
 (function () {
   'use strict';
-
+Lampa.Platform.tv();
   function _classCallCheck(instance, Constructor) {
     if (!(instance instanceof Constructor)) {
       throw new TypeError("Cannot call a class as a function");
