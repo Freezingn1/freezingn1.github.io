80424eb8290a0e28394b8f3c3f160dd2cfa68fbd
Freezingn1 <edikgarr@gmail.com>
2025-04-12 16:15:51 +0300

    Add files via upload

--- TESTSURR.js
+++ TESTSURR.js
@@ -2,7 +2,7 @@
   'use strict';
 
 window.lampainit_invc.appload = function appload() {
-Lampa.Utils.putScriptAsync(["https://aviamovie.github.io/surs.js"]);
+Lampa.Utils.putScriptAsync(["https://lam.maxvol.pro/plugins/surs.js"]);
 
     Lampa.Storage.set('surs_disableCustomName', true);
     Lampa.Storage.set('surs_name', 'AVIAMOVIE');
