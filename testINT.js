68ff54a0c3ff44c88b97867440ea6bec9f9c21ae
Freezingn1 <edikgarr@gmail.com>
2025-05-03 20:44:32 +0300

    Add files via upload

--- testINT.js
+++ testINT.js
@@ -300,7 +300,7 @@
         };
 
         this.background = function (elem) {
-            var new_background = Lampa.Api.img(elem.backdrop_path, 'w1280');
+            var new_background = Lampa.Api.img(elem.backdrop_path, 'w1920');
             clearTimeout(background_timer);
             if (new_background == background_last) return;
             
