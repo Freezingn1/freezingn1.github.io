7406c0ac2cdfe954f46ae45b8938503f9e010fac
Freezingn1 <edikgarr@gmail.com>
2025-05-03 23:58:11 +0300

    Add files via upload

--- testINT.js
+++ testINT.js
@@ -44,7 +44,7 @@
 
                 // Очищаем предыдущий заголовок перед загрузкой нового
                 html.find('.new-interface-info__title').empty();
-
+create():
 const MAX_CACHE_SIZE = 10;
 if (Object.keys(logoCache).length >= MAX_CACHE_SIZE) {
     const oldestKey = Object.keys(logoCache)[0];
