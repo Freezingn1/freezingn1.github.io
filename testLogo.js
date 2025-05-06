17951fcaa7e84cbef62390c9e3f72274aec5258f
Freezingn1 <edikgarr@gmail.com>
2025-05-07 00:25:14 +0300

    Add files via upload

--- testLogo.js
+++ testLogo.js
@@ -161,14 +161,12 @@
                     if (title) {
                         render.find(".full-start-new__rate-line").first().before(`
                             <div class="ru-title-full" style="
-                                position: absolute;
-								top: -30px;
-								right: 0;
-								color: #ffffff;
-								font-weight: 500;
-								opacity: 0.80;
-								max-width: 300px;
-								text-align: right;
+                                color: #ffffff;
+                                font-weight: 500;
+                                text-align: right;
+                                margin-bottom: 10px;
+                                opacity: 0.80;
+                                max-width: 500px;
                             ">
                                 RU: ${title}
                             </div>
