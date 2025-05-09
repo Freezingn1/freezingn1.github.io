4ccfacd1fbe110d4ec9a6cdcc5fc09f0c0855982
Freezingn1 <edikgarr@gmail.com>
2025-05-09 16:33:24 +0300

    Add files via upload

--- anime.js
+++ anime.js
@@ -6,25 +6,87 @@
         
         // Добавляем обработчик клика
         menuItem.on('click', function() {
-            // Здесь можно добавить логику при клике на пункт меню
-            console.log('Anime2 menu item clicked');
+            // Скрываем все активные разделы
+            $('.activity--active').removeClass('activity--active');
+            
+            // Создаем контент для нового раздела
+            var content = `
+            <div class="activity layer--width activity--active">
+                <div class="activity__body">
+                    <div class="new-interface">
+                        <img class="full-start__background loaded" src="https://image.tmdb.org/t/p/w1280//89qSKhLrJOUhp6xgbqgSTpzblbA.jpg">
+                        <div class="new-interface-info">
+                            <div class="new-interface-info__body">
+                                <div class="new-interface-info__head"><span>2025</span>, Китай</div>
+                                <div class="new-interface-info__title"><img class="new-interface-logo" src="https://image.tmdb.org//t/p/w500/1FIvQSUzFovFdk8rNXnE9t76G6E.png" alt="Сверхкуб" onerror="this.onerror=null;this.parentElement.textContent='Сверхкуб'"></div>
+                                <div class="new-interface-info__details">
+                                    <div class="full-start__rate"><div>9.1</div><div>TMDB</div></div>
+                                    <span class="new-interface-info__split">●</span>
+                                    <span class="full-start__pg">Эпизодов 12</span>
+                                    <span class="new-interface-info__split">●</span>
+                                    <span class="full-start__pg" style="font-size: 0.9em;">14+</span>
+                                </div>
+                                <div class="new-interface-info__description"></div>
+                            </div>
+                        </div>
+                        <div class="scroll scroll--mask scroll--over layer--wheight" style="height: 239.281px;">
+                            <div class="scroll__content">
+                                <div class="scroll__body">
+                                    <!-- Здесь можно добавить блоки с контентом -->
+                                    <div class="items-line layer--visible layer--render items-line--type-none">
+                                        <div class="items-line__head">
+                                            <div class="items-line__title">Популярное в Anime2</div>
+                                            <div class="items-line__more selector">Еще</div>
+                                        </div>
+                                        <div class="items-line__body">
+                                            <div class="scroll scroll--horizontal">
+                                                <div class="scroll__content">
+                                                    <div class="scroll__body items-cards">
+                                                        <!-- Карточки контента -->
+                                                        <div class="card selector layer--visible layer--render card--tv card--small card--wide card--loaded">
+                                                            <div class="card__view">
+                                                                <img src="https://image.tmdb.org/t/p/w780//89qSKhLrJOUhp6xgbqgSTpzblbA.jpg" class="card__img">
+                                                                <div class="card__icons"><div class="card__icons-inner"></div></div>
+                                                                <div class="card__type" style="font-size: 0.9em;">Сериал</div>
+                                                                <div class="card__vote">9.1</div>
+                                                                <div class="card__quality"><div>webrip</div></div>
+                                                            </div>
+                                                        </div>
+                                                        <!-- Добавьте больше карточек по аналогии -->
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="activity__loader"></div>
+            </div>
+            `;
+            
+            // Добавляем контент в обертку
+            $('.wrap__content').append(content);
+            
+            // Имитируем загрузку данных (можно заменить на реальный API запрос)
+            setTimeout(function() {
+                $('.activity__loader').hide();
+            }, 1000);
         });
         
-        // Ищем меню и добавляем наш пункт
-        var menuList = $('.menu__list');
-        if (menuList.length) {
-            menuList.append(menuItem);
-        } else {
-            console.warn('Menu list not found');
-            
-            // Альтернативный вариант, если меню не найдено сразу
-            var checkExist = setInterval(function() {
-                menuList = $('.menu__list');
-                if (menuList.length) {
-                    clearInterval(checkExist);
-                    menuList.append(menuItem);
-                }
-            }, 100); // проверяем каждые 100мс
+        // Функция для поиска и добавления пункта меню
+        function addMenuItem() {
+            var menuList = $('.menu__list');
+            if (menuList.length) {
+                menuList.append(menuItem);
+            } else {
+                setTimeout(addMenuItem, 100);
+            }
         }
+        
+        // Начинаем попытки добавить пункт меню
+        addMenuItem();
     });
 })();
