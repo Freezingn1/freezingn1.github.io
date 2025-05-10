9099d42e7566fe947148b8dd8e964d654b8600ac
Freezingn1 <edikgarr@gmail.com>
2025-05-10 16:55:17 +0300

    Add files via upload

--- theme-dark.js
+++ theme-dark.js
@@ -5,6 +5,12 @@
 	Lampa.Utils.putScriptAsync(['https://freezingn1.github.io/testLogo.js'], function () { });
 	Lampa.Utils.putScriptAsync(['https://freezingn1.github.io/testCardify.js'], function () { });
 	Lampa.Utils.putScriptAsync(['https://freezingn1.github.io/testINT.js'], function () { });
+	Lampa.Utils.putScriptAsync(['https://freezingn1.github.io/Fpubtorr.js'], function () { });
+	Lampa.Utils.putScriptAsync(['https://bylampa.github.io/lable_serial.js'], function () { });
+	Lampa.Utils.putScriptAsync(['https://freezingn1.github.io/cinema.js'], function () { });
+	Lampa.Utils.putScriptAsync(['https://reyohoho.space:4437/static/lampa.js'], function () { });
+	Lampa.Utils.putScriptAsync(['https://aviamovie.github.io/surs.js'], function () { });
+	Lampa.Utils.putScriptAsync(['https://and7ey.github.io/lampa/head_filter.js'], function () { });
 
 })();
 
