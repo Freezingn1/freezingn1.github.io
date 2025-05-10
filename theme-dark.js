22c2b7ebdec42a01c4ed98f5b18fca810b95ee45
Freezingn1 <edikgarr@gmail.com>
2025-05-10 16:51:44 +0300

    Add files via upload

---
+++ theme-dark.js
@@ -0,0 +1,10 @@
+(function () {
+    'use strict';
+
+    Lampa.Utils.putScriptAsync(['https://freezingn1.github.io/dark.js'], function () { });
+	Lampa.Utils.putScriptAsync(['https://freezingn1.github.io/testLogo.js'], function () { });
+	Lampa.Utils.putScriptAsync(['https://freezingn1.github.io/testCardify.js'], function () { });
+	Lampa.Utils.putScriptAsync(['https://freezingn1.github.io/testINT.js'], function () { });
+
+})();
+
