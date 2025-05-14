d534f7d27d12af98894eefcc982674d0be050065
Freezingn1 <edikgarr@gmail.com>
2025-05-14 23:36:05 +0300

    Update skaz.js

--- skaz.js
+++ skaz.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
 
-    Lampa.Utils.putScriptAsync(['https://skaz.tv/vcdn.js'], function () { });
+    Lampa.Utils.putScriptAsync(['https://skaz.tv/onlines.js'], function () { });
 
 })();
 
