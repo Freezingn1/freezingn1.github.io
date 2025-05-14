8c55257551e209b8669b4994680686952b23ff30
Freezingn1 <edikgarr@gmail.com>
2025-05-14 23:30:49 +0300

    Update theme-dark.js

--- theme-dark.js
+++ theme-dark.js
@@ -1,10 +1,10 @@
 (function () {
     'use strict';
 
-    Lampa.Utils.putScriptAsync(['https://freezingn1.github.io/dark.js'], function () { });
-	Lampa.Utils.putScriptAsync(['https://freezingn1.github.io/testLogo.js'], function () { });
-	Lampa.Utils.putScriptAsync(['https://freezingn1.github.io/testCardify.js'], function () { });
-	Lampa.Utils.putScriptAsync(['https://freezingn1.github.io/testINT.js'], function () { });
+    Lampa.Utils.putScriptAsync(['https://freezingn1.github.io/dark_style.js'], function () { });
+	Lampa.Utils.putScriptAsync(['https://freezingn1.github.io/logo.js'], function () { });
+	Lampa.Utils.putScriptAsync(['https://freezingn1.github.io/cardify.js'], function () { });
+	Lampa.Utils.putScriptAsync(['https://freezingn1.github.io/interface.js'], function () { });
 	Lampa.Utils.putScriptAsync(['https://freezingn1.github.io/Fpubtorr.js'], function () { });
 	Lampa.Utils.putScriptAsync(['https://bylampa.github.io/lable_serial.js'], function () { });
 	Lampa.Utils.putScriptAsync(['https://freezingn1.github.io/cinema.js'], function () { });
