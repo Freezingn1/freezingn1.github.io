09063efb55eefee7a41137a4f80c303249489ca4
Freezingn1 <edikgarr@gmail.com>
2025-05-15 01:38:31 +0300

    Add files via upload

--- logo_title.js
+++ logo_title.js
@@ -1,15 +1,15 @@
 !function() {
     "use strict";
-    
-    Lampa.SettingsApi.addComponent({
-        component: 'logo_nazvanie',
-        name: Lampa.Lang.translate('Интерфейс карточек'),
-        icon: `
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
-        </svg>
-        `
-        });
+	
+	Lampa.SettingsApi.addComponent({
+		component: 'logo_nazvanie',
+		name: Lampa.Lang.translate('Интерфейс карточек'),
+		icon: `
+		<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+		<path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+		</svg>
+		`
+		});
 
     // Настройки логотипов
     Lampa.SettingsApi.addParam({
@@ -53,7 +53,7 @@
         window.logoplugin = true;
 
         const TMDB_API_KEY = "4ef0d7355d9ffb5151e987764708ce96";
-        const TMDB_API_URL = "https://api.themoviedb.org/3"; // Исправлено: реальный URL API
+        const TMDB_API_URL = "https://api.themoviedb.org/3";
         const titleCache = new Map();
 
         // Функция для выбора лучшего логотипа
