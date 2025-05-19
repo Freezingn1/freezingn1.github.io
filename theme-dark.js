5113326b5cd6cfacefd01724ab5d2a733dc94cf8
Freezingn1 <edikgarr@gmail.com>
2025-05-19 20:48:00 +0300

    Update theme-dark.js

--- theme-dark.js
+++ theme-dark.js
@@ -1,15 +1,34 @@
 (function () {
     'use strict';
 
-    Lampa.Utils.putScriptAsync(['https://freezingn1.github.io/dark_style.js'], function () { });
+	//Тёмная тема
+        Lampa.Utils.putScriptAsync(['https://freezingn1.github.io/dark_style.js'], function () { });
+
+	//Логотипы в карточках
 	Lampa.Utils.putScriptAsync(['https://freezingn1.github.io/logo_title.js'], function () { });
+
+	//Cardify
 	Lampa.Utils.putScriptAsync(['https://freezingn1.github.io/cardify.js'], function () { });
+
+	//Стильный интерфейс
 	Lampa.Utils.putScriptAsync(['https://freezingn1.github.io/interface.js'], function () { });
+
+	//Каталог парсеров
 	Lampa.Utils.putScriptAsync(['https://freezingn1.github.io/Fpubtorr.js'], function () { });
+
+	//Онлайн
 	Lampa.Utils.putScriptAsync(['https://freezingn1.github.io/cinema.js'], function () { });
+
+	//HDRezka
 	Lampa.Utils.putScriptAsync(['https://reyohoho.space:4437/static/lampa.js'], function () { });
+
+	//Подборки
 	Lampa.Utils.putScriptAsync(['https://aviamovie.github.io/surs.js'], function () { });
+
+	//Настройка шапки
 	Lampa.Utils.putScriptAsync(['https://and7ey.github.io/lampa/head_filter.js'], function () { });
+
+	//"Сериал" вместо "TV"
 	Lampa.Utils.putScriptAsync(['https://freezingn1.github.io/lable_serial.js'], function () { });
 
 })();
