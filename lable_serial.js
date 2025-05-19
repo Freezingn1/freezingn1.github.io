d11226a23c4455bd32d37ce7d25fdef2bb0b4fb6
Freezingn1 <edikgarr@gmail.com>
2025-05-19 14:24:59 +0300

    Add files via upload

--- lable_serial.js
+++ lable_serial.js
@@ -3,7 +3,7 @@ function modifyCardType() {
   document.querySelectorAll('.card__type').forEach(el => {
     if (el.textContent.trim() === 'TV') el.textContent = 'Сериал';
     
-    el.style.marginLeft = '0.1em';
+    el.style.marginLeft = '0.5em';
     el.style.fontSize = '0.9em';
   });
 }
