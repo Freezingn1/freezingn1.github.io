8ff8c32354bd3f8cce55455f5458aad6855463c1
Freezingn1 <edikgarr@gmail.com>
2025-05-20 14:05:43 +0300

    Add files via upload

--- myown_anime.js
+++ myown_anime.js
@@ -125,10 +125,10 @@
           var parts_limit = 6;
           var current_year = new Date().getFullYear();
           var parts_data = [
-		  // ТЕСТ
+		  // Популярные аниме 2020–2026 с высоким рейтингом
             function (call) {
               owner.get('discover/tv?with_original_language=ja&with_genres=16&without_genres=10762&first_air_date.gte=2020-01-01&first_air_date.lte=2026-12-31&sort_by=popularity.desc&vote_average.gte=7.0&vote_count.gte=20', params, function (json) {
-                json.title = 'ТЕСТ';
+                json.title = 'Популярные аниме с высоким рейтингом';
 
                 call(json);
               }, call);
