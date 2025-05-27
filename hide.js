94dc129e8b98b987b38ab6b07e3d7756103ae9f9
Freezingn1 <edikgarr@gmail.com>
2025-05-27 14:54:11 +0300

    Add files via upload

--- hide.js
+++ hide.js
@@ -1,10 +1,5 @@
-const observer = new MutationObserver(() => {
-    const elements = document.querySelectorAll('.settings-folder.selector[data-component="player"], .settings-folder.selector[data-component="parental_control"], .settings-folder.selector[data-component="filmix"]');
-    
-    if (elements.length) {
-        elements.forEach(el => el.style.display = 'none');
-        observer.disconnect(); // Останавливаем после скрытия
-    }
-});
-
-observer.observe(document.body, { childList: true, subtree: true });
+setTimeout(() => {
+    document.querySelectorAll('.settings-folder.selector[data-component="player"], .settings-folder.selector[data-component="parental_control"], .settings-folder.selector[data-component="filmix"]').forEach(el => {
+        el.style.display = 'none';
+    });
+}, 1000); // Ждём 1 секунду перед выполнением
