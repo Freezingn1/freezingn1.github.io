f71a3b4ea056dece838104949b6fb9597079861e
Freezingn1 <edikgarr@gmail.com>
2025-05-27 14:53:06 +0300

    Add files via upload

--- hide.js
+++ hide.js
@@ -1,4 +1,10 @@
-// Скрываем элементы по data-component
-document.querySelectorAll('.settings-folder.selector[data-component="player"], .settings-folder.selector[data-component="parental_control"], .settings-folder.selector[data-component="filmix"]').forEach(el => {
-    el.style.display = 'none';
+const observer = new MutationObserver(() => {
+    const elements = document.querySelectorAll('.settings-folder.selector[data-component="player"], .settings-folder.selector[data-component="parental_control"], .settings-folder.selector[data-component="filmix"]');
+    
+    if (elements.length) {
+        elements.forEach(el => el.style.display = 'none');
+        observer.disconnect(); // Останавливаем после скрытия
+    }
 });
+
+observer.observe(document.body, { childList: true, subtree: true });
