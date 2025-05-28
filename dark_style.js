f6a0e054a570cecc62a4d2af46f02c61d6ead261
Freezingn1 <edikgarr@gmail.com>
2025-05-28 23:06:24 +0300

    Add files via upload

--- dark_style.js
+++ dark_style.js
@@ -208,6 +208,11 @@
             .extensions__item {
                 background-color: #181818;
             }
+			
+			.modal {
+				-webkit-backdrop-filter: blur(0.5em);
+				backdrop-filter: blur(0.5em);
+			}
 
             .extensions__item.focus:after {
                 border: 0.3em solid #c22222;
