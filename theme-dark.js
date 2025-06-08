23950a752164ff8e01dcc626c780762197fa239d
Freezingn1 <edikgarr@gmail.com>
2025-06-09 00:28:49 +0300

    Update theme-dark.js

--- theme-dark.js
+++ theme-dark.js
@@ -1,26 +1,11 @@
 (function () {
     'use strict';
 
-	//Тёмная тема
-        Lampa.Utils.putScriptAsync(['https://freezingn1.github.io/dark_style.js'], function () { });
-
-	//Логотипы в карточках
-	Lampa.Utils.putScriptAsync(['https://freezingn1.github.io/logo_title.js'], function () { });
-
-	//Cardify
-	Lampa.Utils.putScriptAsync(['https://freezingn1.github.io/cardify.js'], function () { });
-
 	//Стильный интерфейс
 	Lampa.Utils.putScriptAsync(['https://freezingn1.github.io/interface.js'], function () { });
-
-	//Каталог парсеров
-	Lampa.Utils.putScriptAsync(['https://freezingn1.github.io/pubtorr.js'], function () { });
-
-	//Онлайн
-	Lampa.Utils.putScriptAsync(['https://freezingn1.github.io/cinema.js'], function () { });
-
-	//HDRezka
-	Lampa.Utils.putScriptAsync(['https://freezingn1.github.io/hdrezka.js'], function () { });
+	
+	//Тёмная тема
+        Lampa.Utils.putScriptAsync(['https://freezingn1.github.io/dark_style.js'], function () { });
 
 	//Подборки
 	Lampa.Utils.putScriptAsync(['https://freezingn1.github.io/surs.js'], function () { });
@@ -30,6 +15,21 @@
 
 	//"Сериал" вместо "TV"
 	Lampa.Utils.putScriptAsync(['https://freezingn1.github.io/lable_serial.js'], function () { });
+
+	//Логотипы в карточках
+	Lampa.Utils.putScriptAsync(['https://freezingn1.github.io/logo_title.js'], function () { });
+
+	//Cardify
+	Lampa.Utils.putScriptAsync(['https://freezingn1.github.io/cardify.js'], function () { });
+
+	//Каталог парсеров
+	Lampa.Utils.putScriptAsync(['https://freezingn1.github.io/pubtorr.js'], function () { });
+
+	//Онлайн
+	Lampa.Utils.putScriptAsync(['https://freezingn1.github.io/cinema.js'], function () { });
+
+	//HDRezka
+	Lampa.Utils.putScriptAsync(['https://freezingn1.github.io/hdrezka.js'], function () { });
 
 })();
 
