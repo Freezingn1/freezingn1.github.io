454930be7956705848080508e5c0764269e3a599
Freezingn1 <edikgarr@gmail.com>
2025-06-09 02:00:12 +0300

    Add files via upload

--- surs.js
+++ surs.js
@@ -421,24 +421,29 @@ function startPlugin() {
             }
 
             var partsData = [
-                function (callback) {
-                    var baseUrl = 'trending/all/week';
-                    baseUrl = applyAgeRestriction(baseUrl);
+        function (callback) {
+            var baseUrl = 'trending/all/week';
+            baseUrl = applyAgeRestriction(baseUrl);
 
-                    owner.get(baseUrl, params, function (json) {
-                        if (json.results) {
-                            json.results = json.results.filter(function (result) {
-                                var forbiddenCountries = ['KR', 'CN', 'JP'];
-                                return !result.origin_country || !result.origin_country.some(function (country) {
-                                    return forbiddenCountries.includes(country);
-                                });
-                            });
-                        }
-                        json.title = Lampa.Lang.translate('surs_title_trend_week');
-                        callback(json);
-                    }, callback);
+            owner.get(baseUrl, params, function (json) {
+                if (json.results) {
+                    json.results = json.results.filter(function (result) {
+                        var forbiddenCountries = ['KR', 'CN', 'JP'];
+                        return !result.origin_country || !result.origin_country.some(function (country) {
+                            return forbiddenCountries.includes(country);
+                        });
+                    });
+                    
+                    // Добавьте эту проверку и вызов shuffleArray
+                    if (getStoredSetting('shuffleTrending', true)) {
+                        shuffleArray(json.results);
+                    }
                 }
-            ];
+                json.title = Lampa.Lang.translate('surs_title_trend_week');
+                callback(json);
+            }, callback);
+        }
+    ];
 
             var CustomData = [];
 
