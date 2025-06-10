89ce383d9495499ad7d3fbdb7b6108c97f3b0a58
Freezingn1 <edikgarr@gmail.com>
2025-06-10 18:09:22 +0300

    Add files via upload

---
+++ tmdbproxy.js
@@ -0,0 +1,19 @@
+(function () {
+    'use strict';
+
+    Lampa.TMDB.image = function (url) {
+        var base = Lampa.Utils.protocol() + 'image.tmdb.org/' + url;
+        return Lampa.Storage.field('proxy_tmdb') ? 'lam.maxvol.pro/tmdb/img/' + url : base;
+    };
+
+    Lampa.TMDB.api = function (url) {
+        var base = Lampa.Utils.protocol() + 'api.themoviedb.org/3/' + url;
+        return Lampa.Storage.field('proxy_tmdb') ? 'lam.maxvol.pro/tmdb/api/3/' + url : base;
+    };
+
+    Lampa.Settings.listener.follow('open', function (e) {
+        if (e.name == 'tmdb') {
+            e.body.find('[data-parent="proxy"]').remove();
+        }
+    });
+})();
