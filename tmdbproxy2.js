273a0e8a9c1d0a904a5d4023eaa1eccc8f484990
Freezingn1 <edikgarr@gmail.com>
2025-06-11 16:57:05 +0300

    Update tmdbproxy2.js

--- tmdbproxy2.js
+++ tmdbproxy2.js
@@ -3,13 +3,13 @@
 
     Lampa.TMDB.image = function (url) {
         var base = Lampa.Utils.protocol() + 'image.tmdb.org/' + url;
-        return Lampa.Storage.field('proxy_tmdb') ? 'https://lam.maxvol.pro/tmdb/img/' + url : base;
+        return Lampa.Storage.field('proxy_tmdb') ? 'http://lam.maxvol.pro/tmdb/img/' + url : base;
     };
 	
 
     Lampa.TMDB.api = function (url) {
         var base = Lampa.Utils.protocol() + 'api.themoviedb.org/3/' + url;
-        return Lampa.Storage.field('proxy_tmdb') ? 'https://lam.maxvol.pro/tmdb/api/3/' + url : base;
+        return Lampa.Storage.field('proxy_tmdb') ? 'http://lam.maxvol.pro/tmdb/api/3/' + url : base;
     };
 
     Lampa.Settings.listener.follow('open', function (e) {
