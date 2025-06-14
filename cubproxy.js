46f6b6cac2233fee0f43adb1f3137d655c5c5224
Freezingn1 <edikgarr@gmail.com>
2025-06-14 22:38:01 +0300

    Add files via upload

--- cubproxy.js
+++ cubproxy.js
@@ -2,15 +2,13 @@
   'use strict';
 
   Lampa.Listener.follow('request_before', function(e) {
-    var proxy_url = 'http://lampa.byskaz.ru/cub/tmdb./'; // Прокси для tmdb.cub.red
-    var original_domain = 'tmdb.cub.red/'; // Оригинальный домен, который нужно проксировать
+    var proxy_url = 'http://lampa.byskaz.ru/cub/tmdb./';
+    var need_proxy = Lampa.Manifest.cub_mirrors.find(function(mirror) {
+      return e.params.url.indexOf(mirror) > -1;
+    });
 
-    // Проверяем, содержит ли URL оригинальный домен и не является ли уже проксированным
-    if (e.params.url.indexOf(original_domain) > -1 && e.params.url.indexOf('/cub/tmdb/') === -1) {
-      // Удаляем протокол и оригинальный домен, оставляя только путь
-      var path = e.params.url.replace(/^https?:\/\//, '').replace(original_domain, '');
-      // Формируем новый URL с прокси
-      e.params.url = proxy_url + path;
+    if (need_proxy && e.params.url.indexOf('tmdb.cub.red/') == -1) {
+      e.params.url = proxy_url + e.params.url.replace(/^https?:\/\//, '');
     }
   });
 
