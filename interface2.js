4795988d11a26cf59238c73424606300b08de139
Freezingn1 <edikgarr@gmail.com>
2025-06-20 00:51:11 +0300

    Add files via upload

--- interface2.js
+++ interface2.js
@@ -151,7 +151,7 @@
                 if (isDestroyed || !html) return;
                 titleElement.text(data.title);
             };
-        }, 300);
+        }, 1000);
       };
 
       this.draw = function (data) {
