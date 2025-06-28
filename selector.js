578072266374de85e4619abbcd9ada3563c87cf2
Freezingn1 <edikgarr@gmail.com>
2025-06-28 23:27:04 +0300

    Delete selector.js

--- selector.js
+++
@@ -1,7 +0,0 @@
-// Найти все элементы с обоими классами
-const elements = document.querySelectorAll('.full-descr__text.selector');
-
-// Удалить класс 'selector' у каждого элемента
-elements.forEach(el => {
-    el.classList.remove('selector');
-});
