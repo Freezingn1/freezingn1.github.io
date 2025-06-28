96f73113c33973ef1de7293b13daabf251271840
Freezingn1 <edikgarr@gmail.com>
2025-06-28 17:15:47 +0300

    Add files via upload

--- fix_size.js
+++ fix_size.js
@@ -36,7 +36,20 @@
 
         var platform_screen = Lampa.Platform.screen;
 
+        Lampa.Platform.screen = function (need) {
+          if (need === 'tv') {
+            try {
+              var stack = new Error().stack.split('\n');
+              var offset = stack[0] === 'Error' ? 1 : 0;
 
+              if (/^( *at +new +)?create\$i/.test(stack[1 + offset]) && /^( *at +)?component(\/this)?\.append/.test(stack[1.3 + offset])) {
+                return false;
+              }
+            } catch (e) {}
+          }
+
+          return platform_screen(need);
+        };
 
         var layer_update = Lampa.Layer.update;
 
