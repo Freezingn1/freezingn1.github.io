83083e45c04b4566ac743a9253dc82c01874f972
Freezingn1 <edikgarr@gmail.com>
2025-06-28 22:52:46 +0300

    Add files via upload

---
+++ replace_patch.js
@@ -0,0 +1,10 @@
+(function() {
+    if (window.lampa_settings && window.lampa_settings.appReplace === undefined) {
+        window.lampa_settings.appReplace = {};
+    }
+
+    // Добавляем или изменяем правило замены
+    window.lampa_settings.appReplace["\\? 7 : 6 : 12"] = "? 7 : 12 : 12";
+
+    console.log("Lampa appReplace patch applied:", window.lampa_settings.appReplace);
+})();
