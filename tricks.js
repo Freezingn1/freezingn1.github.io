7c7c08716db4e7964c2e3b8c00ef199e30e8939d
Freezingn1 <edikgarr@gmail.com>
2025-07-13 12:12:33 +0300

    Add files via upload

--- tricks.js
+++ tricks.js
@@ -1,74 +1,62 @@
 (function () {
     'use strict';
-    Lampa.Platform.tv();
+    Lampa.Platform.tv(); 
     
-    // Константы
-    const SETTINGS_KEY = 'reload_button_enabled';
-    const DEFAULT_VALUE = true;
-
     function add() {
         // Создаем меню для настроек
         Lampa.SettingsApi.addComponent({
             component: 'Multi_Menu_Component',
-            name: 'Tweaks & Tricks',
-            icon: '<svg viewBox="0 0 1024 1024"><path fill="#ffffff" d="M527.58 186.66a120 120 0 1 1-67.95 0V47.54a33.94 33.94 0 0 1 67.95 0v139.12z"/></svg>'
+            name: 'Tweaks &amp; Tricks',
+            icon: '<svg viewBox="0 0 1024 1024" class="icon" version="1.1" xmlns="http://www.w3.org/2000/svg" fill="#000000" stroke="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M527.579429 186.660571a119.954286 119.954286 0 1 1-67.949715 0V47.542857a33.938286 33.938286 0 0 1 67.949715 0v139.190857z m281.380571 604.598858a119.954286 119.954286 0 1 1 67.949714 0v139.190857a33.938286 33.938286 0 1 1-67.949714 0v-139.190857z m-698.441143 0a119.954286 119.954286 0 1 1 67.949714 0v139.190857a33.938286 33.938286 0 0 1-67.949714 0v-139.190857zM144.457143 13.531429c18.797714 0 34.011429 15.213714 34.011428 33.938285v410.038857a33.938286 33.938286 0 0 1-67.949714 0V47.542857c0-18.724571 15.213714-33.938286 33.938286-33.938286z m0 722.139428a60.269714 60.269714 0 1 0 0-120.466286 60.269714 60.269714 0 0 0 0 120.466286z m698.514286-722.139428c18.724571 0 33.938286 15.213714 33.938285 33.938285v410.038857a33.938286 33.938286 0 1 1-67.949714 0V47.542857c0-18.724571 15.213714-33.938286 34.011429-33.938286z m0 722.139428a60.269714 60.269714 0 1 0 0-120.466286 60.269714 60.269714 0 0 0 0 120.466286z m-349.403429 228.717714a33.938286 33.938286 0 0 1-33.938286-33.938285V520.411429a33.938286 33.938286 0 0 1 67.949715 0v410.038857a33.938286 33.938286 0 0 1-34.011429 33.938285z m0-722.139428a60.269714 60.269714 0 1 0 0 120.539428 60.269714 60.269714 0 0 0 0-120.539428z" fill="#ffffff"></path></g></svg>'
         });
 
-        // Добавляем параметр настройки
+        // Добавляем настройку для кнопки перезагрузки
         Lampa.SettingsApi.addParam({
             component: 'Multi_Menu_Component',
             param: {
-                name: SETTINGS_KEY,
-                type: 'trigger',
-                default: DEFAULT_VALUE
+                name: 'Reloadbutton',
+                type: 'toggle',
+                default: true // По умолчанию включено
             },
             field: {
                 name: 'Кнопка перезагрузки',
-                description: 'Показать/скрыть иконку перезагрузки'
+                description: 'Иконка рядом с часами'
             },
             onChange: function(value) {
-                Lampa.Storage.set('settings', SETTINGS_KEY, value);
-                toggleReloadButton(value);
+                if(value) {
+                    $('#RELOAD').removeClass('hide');
+                } else {
+                    $('#RELOAD').addClass('hide');
+                }
+                // Сохраняем настройку
+                Lampa.Storage.set('tweaks_reloadbutton', value);
             }
         });
 
-        // Создаем кнопку
-        function createReloadButton() {
-            const isEnabled = Lampa.Storage.get('settings', SETTINGS_KEY, DEFAULT_VALUE);
-            const buttonHTML = `
-                <div id="RELOAD" class="head__action selector reload-screen ${isEnabled ? '' : 'hide'}">
-                    <svg viewBox="0 0 24 24" fill="#ffffff">
-                        <path d="M4,12a1,1,0,0,1-2,0A9.983,9.983,0,0,1,18.242,4.206V2.758a1,1,0,1,1,2,0v4a1,1,0,0,1-1,1h-4a1,1,0,0,1,0-2h1.743A7.986,7.986,0,0,0,4,12Zm17-1a1,1,0,0,0-1,1A7.986,7.986,0,0,1,7.015,18.242H8.757a1,1,0,1,0,0-2h-4a1,1,0,0,0-1,1v4a1,1,0,0,0,2,0V19.794A9.984,9.984,0,0,0,22,12,1,1,0,0,0,21,11Z"/>
-                    </svg>
-                </div>
-            `;
-            
-            $('div.head__actions').append(buttonHTML);
-            
-            $('#RELOAD').on('hover:enter hover:click hover:touch', function() {
-                location.reload();
-            });
-        }
+        // Получаем сохраненное значение или используем значение по умолчанию
+        var reloadButtonEnabled = Lampa.Storage.get('tweaks_reloadbutton', true);
 
-        // Управление видимостью кнопки
-        function toggleReloadButton(show) {
-            if (show) {
-                $('#RELOAD').removeClass('hide');
-            } else {
-                $('#RELOAD').addClass('hide');
-            }
-        }
+        // Создаем и добавляем кнопку перезагрузки
+        var my_reload = '<div id="RELOAD" class="head__action selector reload-screen' + (reloadButtonEnabled ? '' : ' hide') + '"><svg fill="#ffffff" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff" stroke-width="0.4800000000000001"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M4,12a1,1,0,0,1-2,0A9.983,9.983,0,0,1,18.242,4.206V2.758a1,1,0,1,1,2,0v4a1,1,0,0,1-1,1h-4a1,1,0,0,1,0-2h1.743A7.986,7.986,0,0,0,4,12Zm17-1a1,1,0,0,0-1,1A7.986,7.986,0,0,1,7.015,18.242H8.757a1,1,0,1,0,0-2h-4a1,1,0,0,0-1,1v4a1,1,0,0,0,2,0V19.794A9.984,9.984,0,0,0,22,12,1,1,0,0,0,21,11Z" fill="currentColor"></path></g></svg></div>';
+        $('#app > div.head > div > div.head__actions').append(my_reload);
+        
+        // Обработчик клика для кнопки
+        $('#RELOAD').on('hover:enter hover:click hover:touch', function() {
+            location.reload();
+        });
 
-        // Инициализация кнопки
-        createReloadButton();
+        // Инициализируем состояние кнопки при загрузке
+        if(!reloadButtonEnabled) {
+            $('#RELOAD').addClass('hide');
+        }
     }
 
     // Запуск после готовности приложения
-    if (window.appready) {
+    if(window.appready) {
         add();
     } else {
         Lampa.Listener.follow('app', function(e) {
-            if (e.type === 'ready') add();
+            if(e.type == 'ready') add();
         });
     }
 })();
