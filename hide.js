6e238c8b9ef25ef35c40f650f6918b2d3b342606
Freezingn1 <edikgarr@gmail.com>
2025-07-24 22:36:44 +0300

    Add files via upload

--- hide.js
+++ hide.js
@@ -6,6 +6,7 @@
         const elements = document.querySelectorAll(`
             .settings-folder.selector[data-component="player"],
             .settings-folder.selector[data-component="parental_control"],
+			.settings-folder.selector[data-component="reyohoho_mod"],
             .settings-folder.selector[data-component="filmix"]
         `);
         
