f707bd0255e42f10616bb50b8d49cc92c5ee751c
Freezingn1 <edikgarr@gmail.com>
2025-08-08 18:27:51 +0300

    Add files via upload

--- animelib.js
+++ animelib.js
@@ -1,36 +1,19 @@
 (function () {
-    // Имя твоего прокси
     const PROXY_PREFIX = 'https://wild-mode-68f9.edikgarr.workers.dev/';
+    const ORIGINAL_SRC = Object.getOwnPropertyDescriptor(Image.prototype, 'src');
 
-    Lampa.Listener.follow('full', function (event) {
-        if (event.type === 'complite' && event.data && event.data.movie) {
-            let movie = event.data.movie;
-
-            // Проверяем, есть ли постер и он от cover.imglib.info
-            if (movie && movie.poster && movie.poster.startsWith('https://cover.imglib.info/')) {
-                movie.poster = PROXY_PREFIX + movie.poster;
+    Object.defineProperty(Image.prototype, 'src', {
+        set: function (url) {
+            try {
+                if (typeof url === 'string' && url.startsWith('https://cover.imglib.info/')) {
+                    url = PROXY_PREFIX + url;
+                }
+            } catch (e) {
+                console.error('Proxy image error:', e);
             }
-
-            // Иногда постеры в массиве "background" или "backdrop"
-            if (Array.isArray(movie.background_image)) {
-                movie.background_image = movie.background_image.map(img => {
-                    if (img.startsWith('https://cover.imglib.info/')) {
-                        return PROXY_PREFIX + img;
-                    }
-                    return img;
-                });
-            }
+            return ORIGINAL_SRC.set.call(this, url);
         }
     });
 
-    // Для списка карточек (главные страницы, каталоги)
-    Lampa.Listener.follow('card', function (event) {
-        if (event.type === 'build' && event.data && event.data.poster) {
-            if (event.data.poster.startsWith('https://cover.imglib.info/')) {
-                event.data.poster = PROXY_PREFIX + event.data.poster;
-            }
-        }
-    });
-
-    console.log('Плагин прокси постеров загружен');
+    console.log('Плагин глобального проксирования постеров загружен');
 })();
