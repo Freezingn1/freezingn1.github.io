ca3d3d22c3596f7918a86f48c4a4e748b4db7584
Freezingn1 <edikgarr@gmail.com>
2025-08-08 19:00:59 +0300

    Add files via upload

--- animelib.js
+++ animelib.js
@@ -1,25 +1,12 @@
-(function () {
-    const COVER_HOST = 'https://cover.imglib.info/';
+(function(){
+    const PROXY_PREFIX = 'https://wild-mode-68f9.edikgarr.workers.dev/';
+    const COVER_HOST   = 'https://cover.imglib.info/';
 
-    const obs = new MutationObserver(muts => {
-        muts.forEach(m => {
-            if (m.type === 'childList') {
-                m.addedNodes.forEach(node => {
-                    if (node.tagName === 'IMG') {
-                        console.log('IMG added:', node.src);
-                    } else if (node.querySelectorAll) {
-                        node.querySelectorAll('img').forEach(img => {
-                            console.log('IMG in subtree:', img.src);
-                        });
-                    }
-                });
-            } else if (m.type === 'attributes' && m.attributeName === 'src') {
-                console.log('IMG src changed:', m.target.src);
-            }
-        });
-    });
-
-    obs.observe(document, { childList: true, subtree: true, attributes: true, attributeFilter: ['src'] });
-
-    console.log('Image logger started');
+    const origImgLoad = Lampa.Utils.imgLoad;
+    Lampa.Utils.imgLoad = function(img, url){
+        if (url && url.includes(COVER_HOST) && !url.startsWith(PROXY_PREFIX)) {
+            url = PROXY_PREFIX + url;
+        }
+        return origImgLoad.call(this, img, url);
+    };
 })();
